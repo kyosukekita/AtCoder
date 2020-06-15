8115a8dce82f5313f5c04933bd9f6e9c0d679df5
Kyosuke Kita <54225381+kyosukekita@users.noreply.github.com>
2020-06-16 01:00:20 +0900

    Create B.cpp

---
+++ ABC042/B.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+  
+int main() {
+  int N,L;
+  cin >> N >> L;
+  
+  vector<string> S(N);
+  for(int i=0; i<N;i++){
+    cin >> S[i];
+  }
+  
+  sort(S.begin(), S.end());
+  
+  string s;
+  for(int i=0; i<N;i++){
+    s+=S[i];
+  }
+  
+  cout << s << endl;
+   
+}
