ef7da5d6b994e8975c8a0ed4c52e8c28872abad7
Kyosuke Kita <54225381+kyosukekita@users.noreply.github.com>
2020-06-15 22:57:27 +0900

    Create B.cpp

---
+++ ABC081/B.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  int N;
+  cin >> N;
+  
+  int A[N];
+  for(int i=0; i<N; i++){
+    cin >> A[i];
+  }
+  
+  int count =0;
+  
+  while (true){
+    bool exist_odd= false;
+    for(int i=0; i<N; i++){
+      if (A[i]%2!=0){
+        exist_odd=true;
+      }
+    }
+    
+    if (exist_odd){
+       break;
+    }
+      
+    for(int i=0; i<N; i++){
+       A[i] /=2;
+    }
+    
+    count++;
+  }
+  
+  cout << count << endl;
