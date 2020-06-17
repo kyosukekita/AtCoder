395b74a03efe6ab62340bf862004d91912ae24ba
Kyosuke Kita <54225381+kyosukekita@users.noreply.github.com>
2020-06-17 21:57:24 +0900

    Create A.cpp

---
+++ ABC064/A.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  int r,g,b;
+  cin >> r >> g>> b;
+  
+  int rgb;
+  rgb=100*r+10*g+b;
+  
+  
+  if(rgb%4==0){
+    cout << "YES" <<endl;
+  }else{
+    cout << "NO" <<endl;
+  }
+  
+}
