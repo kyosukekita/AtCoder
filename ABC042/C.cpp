92b4e0d5af8ecdde1f59ce79bd76d5360983fd3c
Kyosuke Kita <54225381+kyosukekita@users.noreply.github.com>
2020-06-18 23:42:27 +0900

    Create C.cpp

---
+++ ABC042/C.cpp
@@ -0,0 +1,36 @@
+//https://atcoder.jp/contests/abc042/submissions/13187994
+
+#include "bits/stdc++.h"
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+ 
+int intToBit(int num){
+  int bit=0;
+  while(1){
+    int tmp;
+    tmp = num%10;
+    bit |= (1<<tmp);
+    num /= 10;
+    if(num == 0) break;
+  }
+  return bit;
+}
+ 
+int main() {
+  int N,K;
+  cin >> N >> K;
+  int hate=0;
+  rep(i,K){
+    int D;
+    cin >> D;
+    hate |= (1<<D);
+  }
+  int res = N;
+  while(1){
+    if(!(intToBit(res) & hate)) break;
+    res++;
+  }
+  cout << res << endl;
+  return 0;
+}
