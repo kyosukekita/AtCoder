44394fd4756a998e54e91ee7c76de3cb94ffcd74
Kyosuke Kita <54225381+kyosukekita@users.noreply.github.com>
2020-06-21 14:45:10 +0900

    Update B.cpp

--- ABC086/B.cpp
+++ ABC086/B.cpp
@@ -5,7 +5,7 @@ int main(){
   int A,B;
   cin >> A>>B;
   
-  int N;
+  int N;// A,Bをstringで受け取って、stoi(A+B)でＮを求めてもよかった。
   if(B<9){N=10*A+B;}
   if(B>9 && B<100){N=100*A+B;}
   if(B==100){N=1000*A+B;}
