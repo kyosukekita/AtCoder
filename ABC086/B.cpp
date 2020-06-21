f0eb59edaa757c74359b150dce90556dbd6f76c6
Kyosuke Kita <54225381+kyosukekita@users.noreply.github.com>
2020-06-21 14:38:16 +0900

    Create B.cpp

---
+++ ABC086/B.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int A,B;
+  cin >> A>>B;
+  
+  int N;
+  if(B<9){N=10*A+B;}
+  if(B>9 && B<100){N=100*A+B;}
+  if(B==100){N=1000*A+B;}
+  
+  bool flag=false;
+  for(int i=0; i*i<=N;i++){
+    if(i*i==N){
+      flag=true;
+    }
+  }
+  
+  if(flag==true){
+    cout << "Yes" << endl;
+  }else{
+    cout << "No" << endl;
+  }
+}
