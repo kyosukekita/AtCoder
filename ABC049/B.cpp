774ca4d9738bd52f1e35ed0d8351d82269169837
Kyosuke Kita <54225381+kyosukekita@users.noreply.github.com>
2020-06-22 22:49:31 +0900

    Create B.cpp

---
+++ ABC049/B.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int h, w;
+	cin >> h >> w;
+	vector<string> c(h);
+	rep(i,h) cin >> c[i];
+	rep(i,h){
+		cout << c[i] << endl;
+		cout << c[i] << endl;
+	}
+ 	return 0;
+}
