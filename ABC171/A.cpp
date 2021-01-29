a23e31a86c10e93e381fa909f2e4cfdd30ca86b3
Kyosuke Kita <54225381+kyosukekita@users.noreply.github.com>
2021-01-29 14:32:06 +0900

    Create A.cpp

---
+++ ABC171/A.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll= long long ;
+
+int main(){
+  char a;
+  cin >> a;
+  
+  if((int(a)>=65) & (int(a)<=90)){
+    cout << "A" << endl;
+  }else{
+    cout << "a" << endl;
+  }
+}  
