8e25f20b06420ce910ef7aff14f6318b40e242d2
Kyosuke Kita <54225381+kyosukekita@users.noreply.github.com>
2021-01-29 09:33:25 +0900

    Create C.cpp

---
+++ ABC169/C.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll= long long ;
+
+int main() {
+  ll A;
+  double B;
+  cin >> A >> B;
+  
+  ll c= B*100+0.001;
+  cout<< A*c/100 << endl;
+}
