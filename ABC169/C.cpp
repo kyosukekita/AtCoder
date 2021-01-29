24feb5ef87ebbc6950d7dd4d38a6d16e8d477104
Kyosuke Kita <54225381+kyosukekita@users.noreply.github.com>
2021-01-29 09:37:09 +0900

    Update C.cpp

--- ABC169/C.cpp
+++ ABC169/C.cpp
@@ -10,3 +10,16 @@ int main() {
   ll c= B*100+0.001;
   cout<< A*c/100 << endl;
 }
+
+
+//文字列として扱うのも確実
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long A;
+    string B;
+    cin >> A >> B;
+    long long B100 = (B[0]-'0')*100 + (B[2]-'0')*10 + (B[3]-'0');
+    cout << A * B100 / 100 << endl;
+}
