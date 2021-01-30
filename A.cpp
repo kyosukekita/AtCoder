9b8bbc168fd2647a54c2749d338808a46debaa2f
Kyosuke Kita <54225381+kyosukekita@users.noreply.github.com>
2021-01-30 22:46:21 +0900

    Create A.cpp

---
+++ A.cpp
@@ -0,0 +1,12 @@
+a,b, c = map(int, input().split())
+
+if c==0:
+    if a>b:
+        print("Takahashi")
+    else:
+        print("Aoki")
+elif c==1:
+    if b>a:
+        print("Aoki")
+    else:
+        print("Takahashi")
