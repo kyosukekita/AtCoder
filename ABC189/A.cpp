691eaa6a99b7a5014ee4a340e5867723f76aa5d6
Kyosuke Kita <54225381+kyosukekita@users.noreply.github.com>
2021-01-30 10:03:54 +0900

    Create A.cpp

---
+++ ABC189/A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll= long long ;
+
+int main(){
+  char A,B,C;
+  cin >> A >> B >> C;
+  
+  if(A==B && B==C){
+    cout << "Won" << endl;
+  }else{
+    cout << "Lost" << endl;
+  }
+  
+}
