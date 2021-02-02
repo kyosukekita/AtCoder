8b131688dd98333521e43b44f9eb19f53b185f59
Kyosuke Kita <54225381+kyosukekita@users.noreply.github.com>
2021-02-02 23:51:05 +0900

    Update D.py

--- ABC138/D.py
+++ ABC138/D.py
@@ -27,3 +27,35 @@ while q:
         q.append(u)
 
 print(*ans)
+
+
+
+
+#TLE &WA
+from collections import deque
+N,Q = map(int, input().split())
+edges=[list(map(int,list(input().split()))) for _ in range(N-1)]
+operation=[list(map(int,list(input().split()))) for _ in range(Q)]
+
+graph=[[] for _ in range(N)]
+for edge in edges:
+  graph[edge[0]-1].append(edge[1]-1)
+
+counter=[0]*N
+def dfs(graph,start,count):
+  visited=[]
+  stack=deque()
+  stack.append(start)
+  while len(stack)>0:
+    next_node=stack.pop()#リストのpopと違って、pop(-1)ができない
+    if next_node in visited:
+      continue
+    visited.append(next_node)
+    counter[next_node]+=count
+    for nei in graph[next_node]:
+      stack.append(nei)
+
+for j in range(Q):
+  dfs(graph,operation[j][0]-1,operation[j][1])
+
+print(' '.join(map(str,counter)))
